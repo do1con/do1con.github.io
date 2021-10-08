aeb1d67a385fd21a82afb6a0ced5c4d208a0f04b
do1con <kss7547@gmail.com>
2021-10-08 10:04:49 +0900

    ADD: typescript, gatsby-plugin-typescript등 추가 EDIT: tsx파일에서 절대경로를 사용할 수 있도록 config 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Kss's Blog`,
+    description: `Passing by...`,
+    author: `@do1con`,
+    siteUrl: ``,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-plugin-typescript",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        isTSX: true,
+        allExtensions: true,
       },
     },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
