8c541a08949672caa5bafc334ca015bf55ceba1a
do1con <kss7547@gmail.com>
2021-10-14 21:29:51 +0900

    REMOVE: Footer 컴포넌트 삭제

--- src/components/Layouts/CommonLayout.tsx
+++ src/components/Layouts/CommonLayout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Header from 'components/Layouts/Header';
-import Footer from 'components/Layouts/Footer';
 
 interface props {
   children?: React.ReactNode;
@@ -20,7 +19,6 @@ const CommonLayout: React.FC<props> = ({ children }: props) => {
         <Header />
         <div className="p-12 mx-auto mt-6 w-full max-w-4xl">{children}</div>
       </ContentWrapper>
-      <Footer />
     </Container>
   );
 };
