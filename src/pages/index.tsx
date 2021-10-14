66374a9f40862ed80d97b36847654140b6f878c4
do1con <kss7547@gmail.com>
2021-10-14 12:51:18 +0900

    EDIT: 인덱스에서 게시글 목록을 불러오기 위해 graphql query 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
 // import GlobalStyle from 'components/Common/GlobalStyle';
 // import Footer from 'components/Layouts/Footer';
 // import Introduction from 'components/Layouts/Header';
 import CommonLayout from 'components/Layouts/CommonLayout';
+import PostList from 'components/Common/PostList';
+import { graphql } from 'gatsby';
 
 const Container = styled.div`
   display: flex;
@@ -11,14 +13,33 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-const IndexPage: FunctionComponent = function () {
+const IndexPage: React.FC = data => {
+  React.useEffect(() => {
+    console.log('여기');
+    console.log('data', data);
+  });
   return (
     <Container>
       <CommonLayout>
-        <div>ㅇㅇㅇㅇ</div>
+        <PostList />
       </CommonLayout>
     </Container>
   );
 };
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
