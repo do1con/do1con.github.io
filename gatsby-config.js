e43cc2a36892f14fcfdeac2fa5e99d75fbbad5d1
do1con <kss7547@gmail.com>
2021-10-14 19:23:51 +0900

    EDIT: markdown 내 이미지의 경로 설정을 도와주는 플러그인들 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,33 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-remark`,
+    `gatsby-remark-images`,
+    // {
+    //   resolve: `gatsby-plugin-mdx`,
+    //   options: {
+    //     gatsbyRemarkPlugins: [
+    //       {
+    //         resolve: `gatsby-remark-images`,
+    //         options: {
+    //           maxWidth: 1200,
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-image`,
   ],
 };
