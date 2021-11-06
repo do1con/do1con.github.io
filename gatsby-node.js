ed1a0d41c99af320e098fe1c4243275989260643
do1con <kss7547@gmail.com>
2021-11-06 19:27:30 +0900

    ADD: RSS 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,3 +19,16 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
     },
   });
 };
+
+const { createFilePath } = require(`gatsby-source-filesystem`);
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    });
+  }
+};
